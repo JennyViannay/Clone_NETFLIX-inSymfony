2453f50cf44bcd9431fceab9b33f8ede1ec9ba4b
JennyViannay <jenny.viannay75@gmail.com>
2020-05-08 19:22:00 +0200

    fix

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -93,7 +93,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
-            return new RedirectResponse($targetPath);
+            return new RedirectResponse($targetPath, 200);
         }
 
         throw new \Exception($this->urlGenerator->generate('home'));
